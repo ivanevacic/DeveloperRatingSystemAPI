ea643375f2697f34e003d9fc270c7970c81e7f39
ivanevacic <ivanevacicweb@gmail.com>
2018-03-09 21:31:38 +0100

    Added test for listing all developers

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,32 @@
-describe('CRUD Developers', () => {
+//  Import request from 'supertest' devdependency
+const request = require('supertest');
+//  Import Chai expect
+const expect = require('chai').expect;
+//  DB Connection
+const knex = require('../db/knex');
+//  Import express app from app.js
+const app = require('../app');
 
+describe('CRUD Developers', () => {
+    //  Run before and other tests are defined
+    before((done) => {
+        //  Run migrations
+        knex.migrate.latest()
+            .then(() => {
+                //  Run seeds
+                return knex.seed.run();
+            }).then(() => done());
+    });
+
+    it('Lists all Records', (done) => {
+        request(app)
+            .get('/api/v1/developers')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('array');
+                done();            
+        });
+    });
 });
