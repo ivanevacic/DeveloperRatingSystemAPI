626d8ec3d481142cbbad3e6fa469dae841d21a04
ivanevacic <ivanevacicweb@gmail.com>
2018-03-10 01:05:45 +0100

    Added testing data and wrote test for post '/' route

--- test/app.test.js
+++ test/app.test.js
@@ -7,6 +7,9 @@ const knex = require('../db/knex');
 //  Import express app from app.js
 const app = require('../app');
 
+//  Testing data
+const fixtures = require('./fixtures');
+
 describe('CRUD Developers', () => {
     //  Run before and other tests are defined
     before((done) => {
@@ -27,6 +30,7 @@ describe('CRUD Developers', () => {
             .expect(200)
             .then((response) => {
                 expect(response.body).to.be.a('array');
+                expect(response.body).to.deep.equal(fixtures.developers);
                 done();            
         });
     });
@@ -40,8 +44,24 @@ describe('CRUD Developers', () => {
             .expect(200)
             .then((response) => {
                 expect(response.body).to.be.a('object');
+                expect(response.body).to.deep.equal(fixtures.developers[0]);
                 done();            
         });
+    });
+
+    //  Test for post '/' route
+    it('Creates developer', (done) => {
+        request(app)
+            .post('/api/v1/developers')
+            .send(fixtures.developer)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object');
+                fixtures.developer.id = response.body.id;
+                expect(response.body).to.deep.equal(fixtures.developer);
+            });
     });
 
 });
---
+++ test/fixtures.js
@@ -0,0 +1,24 @@
+const developers = [
+    {
+        full_name: "Ivan Evačić",
+        position: "Backend Developer"
+    },
+    {
+        full_name: "Luka Kovačić",
+        position: "Frontend Developer"
+    },
+    {
+        full_name: "Lovro Dam",
+        position: "Designer"
+    }
+];
+
+const developer = {
+    "full_name": "Ivan Evačić",
+    "position": "Junior Backend Developer"
+}
+
+module.exports = {
+    developers,
+    developer
+}
